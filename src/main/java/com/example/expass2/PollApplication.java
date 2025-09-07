8ae678ee4d45f70e609c535ebc62e3d237791b88
gwen <whyzaynan@gmail.com>
2025-09-07 16:11:25 +0200

    test

--- src/main/java/com/example/expass2/PollApplication.java
+++ src/main/java/com/example/expass2/PollApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PollApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(PollApplication.class, args);
 	}
